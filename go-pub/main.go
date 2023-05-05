26bc099609b367e4dc803a50c99271f6d7378c13
faringet <mik.davydov14@gmail.com>
2023-05-05 16:21:11 +0300

    pub updated

--- go-pub/main.go
+++ go-pub/main.go
@@ -40,51 +40,55 @@ func main() {
 		return
 	}
 
-	// Генерируем случайное значение orderUid
-	tmp := []byte(order.OrderUid)
-	for i := 0; i < len(order.OrderUid)-4; i++ {
-		if tmp[i] > 0x30 && tmp[i] < 0x39 {
-			tmp[i] = byte(0x30 + rand.Intn(9))
-		} else {
-			tmp[i] = byte(0x61 + rand.Intn(6))
-		}
-	}
-	order.OrderUid = string(tmp)
-	fmt.Println("result orderUid:", order.OrderUid)
+	// Циклом генерим кол-во json'ов
+	for i := 0; i < 5; i++ {
 
-	// Генерируем случайное значение Transaction
-	tmp = []byte(order.Payment.Transaction)
-	for i := 0; i < len(order.OrderUid)-4; i++ {
-		if tmp[i] > 0x30 && tmp[i] < 0x39 {
-			tmp[i] = byte(0x30 + rand.Intn(9))
-		} else {
-			tmp[i] = byte(0x61 + rand.Intn(6))
+		// Генерируем случайное значение orderUid
+		tmp := []byte(order.OrderUid)
+		for i := 0; i < len(order.OrderUid)-4; i++ {
+			if tmp[i] > 0x30 && tmp[i] < 0x39 {
+				tmp[i] = byte(0x30 + rand.Intn(9))
+			} else {
+				tmp[i] = byte(0x61 + rand.Intn(6))
+			}
 		}
-	}
-	order.Payment.Transaction = string(tmp)
-	fmt.Println("result Transaction:", order.Payment.Transaction)
+		order.OrderUid = string(tmp)
+		fmt.Println("result orderUid:", order.OrderUid)
 
-	// Генерируем случайное значение Rid
-	tmp = []byte(order.Items[0].Rid)
-	for i := 0; i < len(order.Items[0].Rid)-4; i++ {
-		if tmp[i] > 0x30 && tmp[i] < 0x39 {
-			tmp[i] = byte(0x30 + rand.Intn(9))
-		} else {
-			tmp[i] = byte(0x61 + rand.Intn(6))
+		// Генерируем случайное значение Transaction
+		tmp = []byte(order.Payment.Transaction)
+		for i := 0; i < len(order.OrderUid)-4; i++ {
+			if tmp[i] > 0x30 && tmp[i] < 0x39 {
+				tmp[i] = byte(0x30 + rand.Intn(9))
+			} else {
+				tmp[i] = byte(0x61 + rand.Intn(6))
+			}
 		}
-	}
-	order.Items[0].Rid = string(tmp)
-	fmt.Println("result Rid:", order.Items[0].Rid)
+		order.Payment.Transaction = string(tmp)
+		fmt.Println("result Transaction:", order.Payment.Transaction)
 
-	msg, err := json.MarshalIndent(order, "", "    ")
-	if err != nil {
-		fmt.Println(err)
-		return
-	} else {
-		err = nc.Publish("event.old", msg)
-		fmt.Println(string(msg))
+		// Генерируем случайное значение Rid
+		tmp = []byte(order.Items[0].Rid)
+		for i := 0; i < len(order.Items[0].Rid)-4; i++ {
+			if tmp[i] > 0x30 && tmp[i] < 0x39 {
+				tmp[i] = byte(0x30 + rand.Intn(9))
+			} else {
+				tmp[i] = byte(0x61 + rand.Intn(6))
+			}
+		}
+		order.Items[0].Rid = string(tmp)
+		fmt.Println("result Rid:", order.Items[0].Rid)
+
+		msg, err := json.MarshalIndent(order, "", "    ")
 		if err != nil {
-			log.Fatalf("Error during publish: %v\n", err)
+			fmt.Println(err)
+			return
+		} else {
+			err = nc.Publish("event.old", msg)
+			fmt.Println(string(msg))
+			if err != nil {
+				log.Fatalf("Error during publish: %v\n", err)
+			}
 		}
 	}
 }
